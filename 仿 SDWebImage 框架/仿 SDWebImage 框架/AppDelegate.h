86cb29d73e8e97218a8f9b3447c2f8e5c55e4699
zhangqingmei <zhangqingmei0904@163.com>
2016-09-17 16:26:01 +0800

    创建项目

---
+++ 仿 SDWebImage 框架/仿 SDWebImage 框架/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  仿 SDWebImage 框架
+//
+//  Created by 张清美 on 16/9/17.
+//  Copyright © 2016年 张清美. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 仿 SDWebImage 框架/仿 SDWebImage 框架/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  仿 SDWebImage 框架
+//
+//  Created by 张清美 on 16/9/17.
+//  Copyright © 2016年 张清美. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
