e2bc718fb30831b666c69cc5007d9f345f32bbbf
zhangqingmei <zhangqingmei0904@163.com>
2016-09-17 16:57:16 +0800

    仿 SD 之 main 方法

--- 仿 SDWebImage 框架/仿 SDWebImage 框架/MapleWebImage/MapleDownloaderOperation.m
+++ 仿 SDWebImage 框架/仿 SDWebImage 框架/MapleWebImage/MapleDownloaderOperation.m
@@ -9,5 +9,13 @@
 #import "MapleDownloaderOperation.h"
 
 @implementation MapleDownloaderOperation
+//任何操作在执行之前都会调用 main 方法:一旦队列调度操作执行,首先自动执行 start 方法 然后调用 main 方法
+//重写 main 方法:所有操作的入口(相当于教室的门)
+-(void)main{
+
+    NSLog(@"%@",[NSThread currentThread]);
+
+
+}
 
 @end
